271cbfcd356cff903d07136d82a0326742b00419
Mutoxicated <blackfalcontv17@gmail.com>
2025-12-27 17:57:03 +0200

    colored vertexes + run fmt

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,8 +1,14 @@
 use std::os::raw::c_void;
 
-use gl::{ARRAY_BUFFER, AttachShader, BindBuffer, BindVertexArray, BufferData, CompileShader, CreateProgram, CreateShader, DeleteBuffers, DeleteProgram, DeleteShader, DeleteVertexArrays, ELEMENT_ARRAY_BUFFER, EnableVertexAttribArray, FALSE, FLOAT, FRAGMENT_SHADER, GenBuffers, GenVertexArrays, LinkProgram, STATIC_DRAW, ShaderSource, UseProgram, VERTEX_SHADER, VertexAttribPointer, types::*};
+use gl::{
+    ARRAY_BUFFER, AttachShader, BindBuffer, BindVertexArray, BufferData, CompileShader,
+    CreateProgram, CreateShader, DeleteBuffers, DeleteProgram, DeleteShader, DeleteVertexArrays,
+    ELEMENT_ARRAY_BUFFER, EnableVertexAttribArray, FRAGMENT_SHADER, GenBuffers,
+    GenVertexArrays, LinkProgram, STATIC_DRAW, ShaderSource,
+    UseProgram, VERTEX_SHADER, VertexAttribPointer, types::*,
+};
 
-use crate::NULL;
+use crate::{COLOR_LAYOUT, NULL, VERTEX_LAYOUT};
 
 pub struct Shader {
     id: GLuint,
@@ -10,26 +16,35 @@ pub struct Shader {
 
 impl Drop for Shader {
     fn drop(&mut self) {
-        unsafe{ DeleteProgram(self.id); }
+        unsafe {
+            DeleteProgram(self.id);
+        }
     }
 }
 
 impl Shader {
-    pub fn init(vertex_file:&str, fragment_file:&str) -> Self {
-        let cwd = std::env::current_dir().unwrap().to_string_lossy().to_string()+"/resources/shaders/";
-        let vertex_source = std::fs::read_to_string(cwd.clone()+vertex_file)
-            .unwrap();
-        let fragment_source = std::fs::read_to_string(cwd.clone()+fragment_file)
-            .unwrap();
-        unsafe{ 
-            let vertex_shader:GLuint = CreateShader(VERTEX_SHADER);
+    pub fn init(vertex_file: &str, fragment_file: &str) -> Self {
+        let cwd = std::env::current_dir()
+            .unwrap()
+            .to_string_lossy()
+            .to_string()
+            + "/resources/shaders/";
+        let vertex_source = std::fs::read_to_string(cwd.clone() + vertex_file).unwrap();
+        let fragment_source = std::fs::read_to_string(cwd.clone() + fragment_file).unwrap();
+        unsafe {
+            let vertex_shader: GLuint = CreateShader(VERTEX_SHADER);
             let c_string = vertex_source.as_ptr() as *const i8;
             ShaderSource(vertex_shader, 1, &c_string as *const *const i8, NULL!(i32));
             CompileShader(vertex_shader);
 
-            let fragment_shader:GLuint = CreateShader(FRAGMENT_SHADER);
+            let fragment_shader: GLuint = CreateShader(FRAGMENT_SHADER);
             let c_string = fragment_source.as_ptr() as *const i8;
-            ShaderSource(fragment_shader, 1, &c_string as *const *const i8, NULL!(i32));
+            ShaderSource(
+                fragment_shader,
+                1,
+                &c_string as *const *const i8,
+                NULL!(i32),
+            );
             CompileShader(fragment_shader);
 
             let id: GLuint = CreateProgram();
@@ -41,31 +56,29 @@ impl Shader {
             DeleteShader(vertex_shader);
             DeleteShader(fragment_shader);
 
-            Self {
-                id,
-            }
+            Self { id }
         }
     }
 
     pub fn activate(&self) {
-        unsafe{ UseProgram(self.id); }
+        unsafe {
+            UseProgram(self.id);
+        }
     }
 }
 
 pub struct Drawer {
     pub vao: GLuint,
     vbo: GLuint,
-    ebo: GLuint
+    ebo: GLuint,
 }
 
 impl Drawer {
-    pub fn init(vertices:Vec<GLfloat>, indices:Vec<GLint>) -> Self {
-        let vertices = vertices.as_slice();
-        let indices = indices.as_slice();
-        let mut instance = Self{
-            vao:0,
-            vbo:0,
-            ebo:0,
+    pub fn init(vertex_data: &[GLfloat], indices: &[GLint]) -> Self {
+        let mut instance: Drawer = Self {
+            vao: 0,
+            vbo: 0,
+            ebo: 0,
         };
 
         unsafe {
@@ -76,20 +89,38 @@ impl Drawer {
             BindVertexArray(instance.vao);
             BindBuffer(ARRAY_BUFFER, instance.vbo);
 
-            BufferData(ARRAY_BUFFER, 
-                size_of_val(vertices).try_into().unwrap(), 
-                vertices.as_ptr() as *const c_void, 
-                STATIC_DRAW);
+            BufferData(
+                ARRAY_BUFFER,
+                size_of_val(vertex_data) as GLsizeiptr,
+                vertex_data.as_ptr() as *const c_void,
+                STATIC_DRAW,
+            );
             BindBuffer(ELEMENT_ARRAY_BUFFER, instance.ebo);
-            BufferData(ELEMENT_ARRAY_BUFFER, 
-                size_of_val(indices).try_into().unwrap(), 
-                indices.as_ptr() as *const c_void, 
-                STATIC_DRAW);
+            BufferData(
+                ELEMENT_ARRAY_BUFFER,
+                size_of_val(indices) as GLsizeiptr,
+                indices.as_ptr() as *const c_void,
+                STATIC_DRAW,
+            );
 
-            VertexAttribPointer(0, 3, FLOAT, FALSE, 
-                0, 
-                NULL!());
-            EnableVertexAttribArray(0);
+            VertexAttribPointer(
+                VERTEX_LAYOUT.0,
+                VERTEX_LAYOUT.1,
+                VERTEX_LAYOUT.2,
+                VERTEX_LAYOUT.3,
+                VERTEX_LAYOUT.4,
+                VERTEX_LAYOUT.5,
+            );
+            EnableVertexAttribArray(VERTEX_LAYOUT.0);
+            VertexAttribPointer(
+                COLOR_LAYOUT.0,
+                COLOR_LAYOUT.1,
+                COLOR_LAYOUT.2,
+                COLOR_LAYOUT.3,
+                COLOR_LAYOUT.4,
+                COLOR_LAYOUT.5,
+            );
+            EnableVertexAttribArray(COLOR_LAYOUT.0);
 
             BindBuffer(ARRAY_BUFFER, 0);
             BindVertexArray(0);
@@ -102,7 +133,7 @@ impl Drawer {
 
 impl Drop for Drawer {
     fn drop(&mut self) {
-        unsafe{
+        unsafe {
             DeleteBuffers(1, &self.ebo);
             DeleteBuffers(1, &self.vbo);
             DeleteVertexArrays(1, &self.vao);
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ mod graphics;
 
 use std::os::raw::c_void;
 
+use gl::{
+    self, BindVertexArray, DrawElements, FALSE, FLOAT, TRIANGLES, UNSIGNED_INT, types::GLfloat,
+};
 use glfw::{Action, Context, GlfwReceiver, Key, fail_on_errors};
-use gl::{self, BindVertexArray, DrawElements, TRIANGLES, UNSIGNED_INT, types::GLfloat};
 use graphics::Shader;
 
 use crate::graphics::Drawer;
@@ -15,10 +17,26 @@ macro_rules! NULL {
     };
     () => {
         std::ptr::null::<std::os::raw::c_void>()
-    }
+    };
 }
 
-// settings
+pub const VERTEX_LAYOUT: (u32, i32, u32, u8, i32, *const c_void) = (
+    0,
+    3,
+    FLOAT,
+    FALSE,
+    7 * size_of::<GLfloat>() as i32,
+    std::ptr::null::<std::os::raw::c_void>(),
+);
+pub const COLOR_LAYOUT: (u32, i32, u32, u8, i32, *const c_void) = (
+    1,
+    4,
+    FLOAT,
+    FALSE,
+    7 * size_of::<GLfloat>() as i32,
+    (3 * size_of::<GLfloat>() as i32) as *const c_void,
+);
+
 const SCR_WIDTH: u32 = 800;
 const SCR_HEIGHT: u32 = 600;
 
@@ -26,12 +44,20 @@ pub fn main() {
     // glfw: initialize and configure
     let mut glfw = glfw::init(glfw::fail_on_errors!()).unwrap();
     glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
     #[cfg(target_os = "macos")]
     glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
 
     // glfw window creation
-    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw
+        .create_window(
+            SCR_WIDTH,
+            SCR_HEIGHT,
+            "LearnOpenGL",
+            glfw::WindowMode::Windowed,
+        )
         .expect("Failed to create GLFW window");
 
     window.make_current();
@@ -41,44 +67,41 @@ pub fn main() {
     // gl: load all OpenGL function pointers
     gl::load_with(|symbol| window.get_proc_address(symbol).unwrap() as *const _);
 
-    let test_vertices = ([
-        -0.5, -0.5, 0.0, // 0
-        0.5, -0.5, 0.0, // 1
-        0.0, 0.5, 0.0, // 2
-  
-        -0.25, 0.0, 0.0, // 3
-        0.0, -0.5, 0.0, // 4
-        0.25, 0.0, 0.0 // 5
-    ] as [GLfloat; 18]).to_vec();
+    let test_vertices = vec![
+        -0.5, -0.5, 0.0,   1.0, 0.0, 0.0, 1.0, // 0
+        0.5, -0.5, 0.0,    1.0, 0.0, 0.0, 1.0, // 1
+        0.0, 0.5, 0.0,     0.0, 0.0, 1.0, 1.0, // 2
+        -0.25, 0.0, 0.0,   0.5, 0.0, 0.5, 1.0, // 3
+        0.0, -0.5, 0.0,    1.0, 0.0, 0.0, 1.0, // 4
+        0.25, 0.0, 0.0,    0.5, 0.0, 0.5, 1.0, // 5
+    ];
 
     /*
-                2
+               2
 
 
-            3       5
+           3       5
 
 
 
-        0       4       1   
-    
-     */
+       0       4       1
 
-    let test_indices = [
-        0, 3, 4, 
-        3, 2, 5,
-        4, 5, 1
-    ].to_vec();
+    */
+
+    let test_indices = [0, 3, 4, 3, 2, 5, 4, 5, 1];
 
     let basic_shader = Shader::init("default.vert", "default.frag");
-    let drawer = Drawer::init(test_vertices, test_indices);
-    
+    let drawer = Drawer::init(&test_vertices, &test_indices);
+
     // render loop
     while !window.should_close() {
         process_events(&mut window, &events);
-        unsafe { gl::Clear(gl::COLOR_BUFFER_BIT); }
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
 
         basic_shader.activate();
-        unsafe{
+        unsafe {
             BindVertexArray(drawer.vao);
             DrawElements(TRIANGLES, 9, UNSIGNED_INT, NULL!(c_void));
         }
@@ -97,7 +120,9 @@ fn process_events(window: &mut glfw::Window, events: &GlfwReceiver<(f64, glfw::W
                 // height will be significantly larger than specified on retina displays.
                 unsafe { gl::Viewport(0, 0, width, height) }
             }
-            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
             _ => {}
         }
     }
